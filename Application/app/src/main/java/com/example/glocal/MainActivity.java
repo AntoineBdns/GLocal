de64e4e3ff5046a23b5e617ec5fc9bf2246cecb4
Slinth <dorian.decroix@hotmail.fr>
2019-05-08 17:26:36 +0200

    ça marche probablement pas mais je peux pas test pour l'instant Unlucky

--- Application/app/src/main/java/com/example/glocal/MainActivity.java
+++ Application/app/src/main/java/com/example/glocal/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.glocal;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
@@ -11,6 +14,22 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainactivity);
+
+        Button map1 = (Button) this.findViewById(R.id.btnMap1);
+        map1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+
+        Button map2 = (Button) this.findViewById(R.id.btnMap2);
+        map2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity2.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
     }
 
 }
